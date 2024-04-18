e7209efe03b9b3b5891d615e7069cc4d13f92752
RodrigoCG <rchuquimia94@gmail.com>
2024-04-18 00:22:01 -0400

    config ng

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    title: 'Login',
+    loadComponent: () => import('./pages/authentication/login/login.component').then(c => c.LoginComponent)
+  },
+  {
+    path: 'register',
+    title: 'Register',
+    loadComponent: () => import('./pages/authentication/register/register.component').then(c => c.RegisterComponent)
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  }
+];
